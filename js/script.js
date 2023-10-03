06dece2adf16bbe2f2e6b6e7f40aa4388ef6e52c
Marian0o1 <agustinsosa1733@hotmail.com>
2023-10-02 22:23:23 -0300

    Correción en fetch

--- js/script.js
+++ js/script.js
@@ -1,42 +1,26 @@
-const xhr = new XMLHttpRequest();
-xhr.open("GET", "../productos.json", true);
-xhr.onreadystatechange = function () {
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    const datos = xhr.responseText;
-    localStorage.setItem("productos", datos);
-  }
-};
-xhr.send();
-async function cargarDatos() {
-  const respuesta = await fetch("../productos.json");
-  const datos = await respuesta.json();
-  localStorage.setItem("productos", JSON.stringify(datos));
-}
-cargarDatos();
-
-/* fetch("productos.json")
+fetch("productos.json")
   .then((respuesta) => respuesta.json())
-  .then((datos) => localStorage.setItem("productos", JSON.stringify(datos))); */
+  .then((datos) => {
+    localStorage.setItem("productos", JSON.stringify(datos));
 
-document.addEventListener("DOMContentLoaded", () => {
-  const gridProductos = document.getElementById("grid-productos");
-  const datosProductos = JSON.parse(localStorage.getItem("productos"));
-  if (datosProductos) {
-    datosProductos.personajes.forEach((personaje) => {
-      const gridItem = document.createElement("div");
-      gridItem.classList.add("grid-item");
-      gridItem.innerHTML = `
+    const gridProductos = document.getElementById("grid-productos");
+    const datosProductos = JSON.parse(localStorage.getItem("productos"));
+    if (datosProductos) {
+      datosProductos.personajes.forEach((personaje) => {
+        const gridItem = document.createElement("div");
+        gridItem.classList.add("grid-item");
+        gridItem.innerHTML = `
       <h4 class="nombre-personaje">${personaje.nombre}</h4>
       <img src= "${personaje.imagen}" alt="${personaje.nombre}" class="img-grid">
       <button class="ver-mas">Ver más</button>
       `;
 
-      gridProductos.appendChild(gridItem);
+        gridProductos.appendChild(gridItem);
 
-      const verMas = gridItem.querySelector(".ver-mas");
-      verMas.addEventListener("click", () => {
-        window.location.href = `productos.html?id=${personaje.id}`;
+        const verMas = gridItem.querySelector(".ver-mas");
+        verMas.addEventListener("click", () => {
+          window.location.href = `productos.html?id=${personaje.id}`;
+        });
       });
-    });
-  }
-});
+    }
+  });
